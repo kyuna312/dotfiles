9fb7275bb673c42d777c1d7f18860f09b7c05a22
maririn312 <khatanzorigb@gmail.com>
2023-10-04 01:24:41 +0800

    macos changes

--- install_brew_cask.sh
+++ install_brew_cask.sh
@@ -8,66 +8,20 @@
 
 # Cask applications
 apps=(
-    betterdisplay              # Retina display management: avibrazil-rdm
-    captin                     # Caps Lock status
-    cheatsheet
-    maccy                      # Clipboard: clipy
-    maczip                     # Compress & extract
-    fliqlo                     # Screen Saver
-    paper                      # Wallpaper
     alt-tab                    # hyperswitch
-    iterm2                     # Terminal
-    # keepingyouawake
-    keycastr                   # Show keys on the screen
-    licecap                    # Recording screen as gif
-    # mounty                     # Mounty for NTFS read/write
     monitorcontrol             # Controls external display brightness and volume
-    mos                        # Smooth and reverse scroll
     raycast                    # Spotlight/Alfred replacement.
-    rectangle                  # Window management
-    hiddenbar                  # Hide menu bar icons. Alternative: bartender, vanilla
-
-    clashx                     # Proxy: v2rayx, shadowsocksx-ng-r
-    iina                       # Media player
-    #microsoft-edge             # Browser: google-chrome
     karabiner-elements         # karabiner: Keboard remapping
     alfred
     utm
     docker
-    # hammersppon                # Ultimate tools
-    # netspot                    # Wifi signal analysis and scanner
+    netspot                    # Wifi signal analysis and scanner
     macfuse                    # File system
-    veracrypt                  # File crypt
-    # foobar2000               # Music player: vox
-    squirrel                   # Pinyin Input: sogouinput
-    handbrake                  # Video Transcoder: transcoder
     warp
-    # Audio
-    # soundflower
-    # soundflowerbed
-
-    # Development
-    # java                       # optional
-    # docker                     # optional
-    # fork                       # Git Client: gitkraken, sourcetree
-    # mark-text                    # Markdown editor: typora
+    docker                     # optional
     visual-studio-code
-
-    # Utilities
-    # aliwangwang
-    # baidunetdisk
     motrix                     # Downloader: ariang
     macgesture
-    qq
-    wechat
-    tencent-lemon
-
-    neteasemusic
-    youdaodict
-    youdaonote
-
-    yesplaymusic
-    zy-player
 )
 
 # Use colors, but only if connected to a terminal, and that terminal
