62afdb1573e53774944a5802b8ac68d0a51bed30
maririn312 <khatanzorigb@gmail.com>
2023-10-07 01:05:28 +0800

    yabai ranger stetchy bar changes

---
+++ .config/sketchybar/plugins/volume.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# The volume_change event supplies a $INFO variable in which the current volume
+# percentage is passed to the script.
+
+if [ "$SENDER" = "volume_change" ]; then
+  VOLUME=$INFO
+
+  case $VOLUME in
+    [6-9][0-9]|100) ICON="墳"
+    ;;
+    [3-5][0-9]) ICON="奔"
+    ;;
+    [1-9]|[1-2][0-9]) ICON="奄"
+    ;;
+    *) ICON="婢"
+  esac
+
+  sketchybar --set $NAME icon="$ICON" label="$VOLUME%"
+fi
