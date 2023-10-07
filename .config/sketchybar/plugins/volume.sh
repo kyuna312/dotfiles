7e870ab140b764dcc68f44e4f46ef75117901150
maririn312 <khatanzorigb@gmail.com>
2023-10-07 23:09:07 +0800

    dotconfig changes

--- .config/sketchybar/plugins/volume.sh
+++ .config/sketchybar/plugins/volume.sh
@@ -1,20 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 
-# The volume_change event supplies a $INFO variable in which the current volume
-# percentage is passed to the script.
+WIDTH=100
 
-if [ "$SENDER" = "volume_change" ]; then
-  VOLUME=$INFO
-
-  case $VOLUME in
-    [6-9][0-9]|100) ICON="墳"
+volume_change() {
+  source "$CONFIG_DIR/icons.sh"
+  case $INFO in
+    [6-9][0-9]|100) ICON=$VOLUME_100
     ;;
-    [3-5][0-9]) ICON="奔"
+    [3-5][0-9]) ICON=$VOLUME_66
     ;;
-    [1-9]|[1-2][0-9]) ICON="奄"
+    [1-2][0-9]) ICON=$VOLUME_33
     ;;
-    *) ICON="婢"
+    [1-9]) ICON=$VOLUME_10
+    ;;
+    0) ICON=$VOLUME_0
+    ;;
+    *) ICON=$VOLUME_100
   esac
 
-  sketchybar --set $NAME icon="$ICON" label="$VOLUME%"
-fi
+  sketchybar --set volume_icon label=$ICON
+
+  sketchybar --set $NAME slider.percentage=$INFO \
+             --animate tanh 30 --set $NAME slider.width=$WIDTH 
+
+  sleep 2
+
+  # Check wether the volume was changed another time while sleeping
+  FINAL_PERCENTAGE=$(sketchybar --query $NAME | jq -r ".slider.percentage")
+  if [ "$FINAL_PERCENTAGE" -eq "$INFO" ]; then
+    sketchybar --animate tanh 30 --set $NAME slider.width=0
+  fi
+}
+
+mouse_clicked() {
+  osascript -e "set volume output volume $PERCENTAGE"
+}
+
+case "$SENDER" in
+  "volume_change") volume_change
+  ;;
+  "mouse.clicked") mouse_clicked
+  ;;
+esac
